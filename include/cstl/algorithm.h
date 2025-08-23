e23cc1cdf30ee6205f9ee4923d9dde85fe7b3b86
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2025-08-23 13:21:44 +0300

    Add heap sort

--- include/cstl/algorithm.h
+++ include/cstl/algorithm.h
@@ -21,7 +21,7 @@ template <typename T> inline void insertion_sort(T *arr, size_t n) noexcept
 
 template <typename T> inline void merge_sort(T *arr, size_t n) noexcept
 {
-    auto internal_sort = [&](auto &&self, T *arr, size_t n) {
+    auto internal_sort = [&](auto &&self, T *arr, size_t n) noexcept -> void {
         if (n <= 1)
             return;
 
@@ -55,7 +55,6 @@ template <typename T> inline void merge_sort(T *arr, size_t n) noexcept
         delete[] R;
     };
 
-
     internal_sort(internal_sort, arr, n);
 }
 
@@ -72,6 +71,42 @@ template <typename T> inline void bubble_sort(T *arr, size_t n) noexcept
                 arr[j] = temp;
             }
         }
+    }
+}
+
+template <typename T> inline void heap_sort(T *arr, size_t n) noexcept
+{
+    auto heapify = [](auto &&self, T *arr, size_t heap_size, size_t i) noexcept -> void {
+        size_t largest = i;
+        size_t left = 2 * i + 1;
+        size_t right = 2 * i + 2;
+
+        if (left < heap_size && arr[left] > arr[largest])
+            largest = left;
+        if (right < heap_size && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            T temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            self(self, arr, heap_size, largest);
+        }
+    };
+
+    for (size_t i = n / 2; i-- > 0;)
+    {
+        heapify(heapify, arr, n, i);
+    }
+
+    for (size_t heap_size = n; heap_size > 1; --heap_size)
+    {
+        T temp = arr[0];
+        arr[0] = arr[heap_size - 1];
+        arr[heap_size - 1] = temp;
+
+        heapify(heapify, arr, heap_size - 1, 0);
     }
 }
 } // namespace cstl
--- include/cstl/memory.h
+++ include/cstl/memory.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "common.h"
+#include <cstl/common.h>
 
 namespace cstl
 {
