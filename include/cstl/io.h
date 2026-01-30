fb97302f8bc8d33fe6b12daa4196e5aceb6ad341
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2026-01-30 20:32:11 +0300

    Update math, add print

---
+++ include/cstl/io.h
@@ -0,0 +1,13 @@
+﻿#pragma
+
+#include <cstl/common.h>
+
+#include <Windows.h>
+
+namespace cstl
+{
+void print(const char *const str)
+{
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), str, static_cast<uint32_t>(strlen(str)), nullptr, nullptr);
+}
+} // namespace cstl
--- include/cstl/math.h
+++ include/cstl/math.h
@@ -1,5 +1,6 @@
-#pragma once
+﻿#pragma once
 
+#include <smmintrin.h>
 #include <xmmintrin.h>
 
 namespace cstl
@@ -9,39 +10,69 @@ constexpr float pi = 3.141592654f;
 constexpr float rad_to_deg = 180.0f / pi;
 constexpr float deg_to_rad = pi / 180.0f;
 
-inline float sqrt(float x)
-{
-    return _mm_cvtss_f32(_mm_sqrt_ss(_mm_set_ss(x)));
-}
+using vector3 = __m128;
+using vector4 = __m128;
 
-struct vector3
+
+struct alignas(16) float3
 {
     float x;
     float y;
     float z;
 
-    vector3() = default;
+    float3() = default;
 
-    vector3(const vector3 &) = default;
-    vector3 &operator=(const vector3 &) = default;
+    float3(const float3 &) = default;
+    float3 &operator=(const float3 &) = default;
 
-    vector3(vector3 &&) = default;
-    vector3 &operator=(vector3 &&) = default;
+    float3(float3 &&) = default;
+    float3 &operator=(float3 &&) = default;
 
-    constexpr vector3(float _x, float _y, float _z) noexcept : x(_x), y(_y), z(_z)
+    constexpr float3(float _x, float _y, float _z) noexcept : x(_x), y(_y), z(_z)
     {
-    }
-
-    inline float length() const noexcept
-    {
-        return sqrt(x * x + y * y + z * z);
-    }
-
-    inline vector3 normal() const noexcept
-    {
-        float inv_l = _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x * x + y * y + z * z)));
-
-        return vector3(x * inv_l, y * inv_l, z * inv_l);
     }
 };
+
+static_assert(sizeof(float3) == 16, "Size of float3 must be 16");
+
+inline float sqrt(float x)
+{
+    return _mm_cvtss_f32(_mm_sqrt_ss(_mm_set_ss(x)));
+}
+
+inline float3 float3_get(const vector3 &v)
+{
+    float3 f;
+    _mm_store_ps(reinterpret_cast<float *>(&f), v);
+    return f;
+}
+
+inline vector3 vector3_get(const float3 &f)
+{
+    return _mm_load_ps(reinterpret_cast<const float *>(&f));
+}
+
+inline float vector3_dot(const vector3 &v1, const vector3 &v2)
+{
+    return _mm_cvtss_f32(_mm_dp_ps(v1, v2, 0x77));
+}
+
+inline float float3_length(const float3 &f)
+{
+    return sqrt(f.x * f.x + f.y * f.y + f.z * f.z);
+}
+
+inline float3 float3_normal(const float3 &f)
+{
+    float inv_l = _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(f.x * f.x + f.y * f.y + f.z * f.z)));
+
+    return float3(f.x * inv_l, f.y * inv_l, f.z * inv_l);
+}
+
+inline float float3_dot(const float3 &f1, const float3 &f2)
+{
+    return vector3_dot(vector3_get(f1), vector3_get(f2));
+}
+
+
 } // namespace cstl
